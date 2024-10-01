cf0003e7e3d6a48dd699eb015a94bd17f8d282b0
Mouad-El-Asri <elasri.mouad.2002@gmail.com>
2024-10-01 16:48:22 +0100

    Implement element access operators and ostream operator for Matrix class

--- Matrix.cpp
+++ Matrix.cpp
@@ -1,17 +1,19 @@
 #include "Matrix.hpp"
 
-Matrix::Matrix(size_t rows, size_t cols, double initValue, bool initMemory)
+Matrix::Matrix(size_t rows, size_t cols)
 	: rows(rows), cols(cols)
 {
 	std::cout << "Matrix default constructor called" << std::endl;
-	if (initMemory) {
-		this->matrix = std::vector<std::vector<double>>(rows, std::vector<double>(cols, initValue));
-	} else {
-		this->matrix = std::vector<std::vector<double>>(rows);
-		for (size_t i = 0; i < rows; i++) {
-			this->matrix[i].reserve(cols);
-		}
+	this->matrix = std::vector<std::vector<double>>(rows);
+	for (size_t i = 0; i < rows; i++) {
+		this->matrix[i].reserve(cols);
 	}
+}
+
+Matrix::Matrix(size_t rows, size_t cols, double initValue)
+	: rows(rows), cols(cols), matrix(rows, std::vector<double>(cols, initValue))
+{
+	std::cout << "Matrix default constructor called" << std::endl;
 }
 
 Matrix::~Matrix() {
@@ -26,16 +28,24 @@ size_t Matrix::getCols() const {
 	return this->cols;
 }
 
-double Matrix::get(size_t row, size_t col) const {
-	if (row >= this->rows || col >= this->cols || row < 0 || col < 0) {
-		throw std::out_of_range("Index out of range");
-	}
+const double &Matrix::operator()(size_t row, size_t col) const {
+	if (row >= this->rows || col >= this->cols || row < 0 || col < 0)
+		throw std::out_of_range("Matrix indices are out of range");
 	return this->matrix[row][col];
 }
 
-void Matrix::set(size_t row, size_t col, double value) {
-	if (row >= this->rows || col >= this->cols || row < 0 || col < 0) {
-		throw std::out_of_range("Index out of range");
+double &Matrix::operator()(size_t row, size_t col) {
+	if (row >= this->rows || col >= this->cols || row < 0 || col < 0)
+		throw std::out_of_range("Matrix indices are out of range");
+	return this->matrix[row][col];
+}
+
+std::ostream& operator<<(std::ostream &os, const Matrix &matrixObj) {
+	for (size_t i = 0; i < matrixObj.rows; i++) {
+		for (size_t j = 0; j < matrixObj.cols; j++){
+			os << matrixObj.matrix[i][j] << " ";
+		}
+		os << std::endl;
 	}
-	this->matrix[row][col] = value;
+	return os;
 }
